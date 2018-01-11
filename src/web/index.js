9a67b94783b85e45fa97ecba92f5c8103a5c0403
Tyler Long <tyler4long@gmail.com>
2018-01-11 15:36:23 +0800

    Step navigation

--- src/web/index.js
+++ src/web/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Steps, Divider, Radio, Input, Form, Button } from 'antd'
+import { Steps, Divider, Radio, Input, Form, Button, Row, Col } from 'antd'
 
 import './index.css'
 
@@ -40,7 +40,7 @@ class App extends React.Component {
                 <Input placeholder='Client Secret' value={this.state.clientSecret} onChange={e => { this.setState({ clientSecret: e.target.value }) }} />
               </Form.Item>
               <Form.Item>
-                <Button type='primary' style={{ width: '100%' }} disabled={this.state.clientId === '' || this.state.clientSecret === ''}>Next</Button>
+                <Button type='primary' style={{ width: '100%' }} disabled={this.state.clientId === '' || this.state.clientSecret === ''} onClick={e => { this.setState({ current: 1 }) }}>Next</Button>
               </Form.Item>
             </Form>
           </React.Fragment>
@@ -48,11 +48,16 @@ class App extends React.Component {
         { this.state.current !== 1 ? null : (
           <div>
             Hello world
+            <Row gutter={16}>
+              <Col span={12}><Button style={{ width: '100%' }} onClick={e => { this.setState({ current: 0 }) }}>Previous</Button></Col>
+              <Col span={12}><Button type='primary' style={{ width: '100%' }} onClick={e => { this.setState({ current: 2 }) }}>Next</Button></Col>
+            </Row>
           </div>
         ) }
         { this.state.current !== 2 ? null : (
           <div>
             Hello world
+            <Button style={{ width: '100%' }} onClick={e => { this.setState({ current: 1 }) }}>Previous</Button>
           </div>
         ) }
       </div>
